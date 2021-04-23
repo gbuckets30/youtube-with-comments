82afbf31046e3b6cb19140e23079cc0992f68191
Utkarsh <saxenautkarsh0@gmail.com>
2021-04-23 18:38:07 +0530

    :fire: Remove import of unnecessary dependencies

--- server/app.js
+++ server/app.js
@@ -4,15 +4,6 @@ const youtube = require("./youtube.js");
 
 const app = express();
 
-// configure website to use ejs templates
-app.set("view engine", "ejs");
-
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.render("index", { videoID: "QZ4BXGgmATU" });
-});
-
 app.post("/", express.urlencoded({ extended: false }), (req, res) => {
   const videoID = req.body.videoID;
   const promise = youtube.getComments(videoID, process.env.YOUTUBE_API);
