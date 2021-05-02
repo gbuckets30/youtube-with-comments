f28db0000d33beacccdb6edba940ca5fff94bc3a
Utkarsh <saxenautkarsh0@gmail.com>
2021-05-02 14:27:33 +0530

    :sparkles: Display comments and video details and statistics

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import YoutubeEmbed from "./components/YoutubeEmbed";
+import Comment from "./components/Comment";
+import VideoDetails from "./components/VideoDetails";
 
 function App() {
   const [videoID, setVideoID] = useState("");
   const [currentVideoID, setCurrentVideoID] = useState("L2vS_050c-M");
+  const [comments, setComments] = useState([]);
+  const [videoDetails, setVideoDetails] = useState({});
 
-  function handleLoadVideoClick(event) {
+  function handleLoadVideoClick() {
     setCurrentVideoID(videoID);
     setVideoID("");
-    event.preventDefault();
   }
 
   function handleInputChange(event) {
@@ -16,17 +19,54 @@ function App() {
     setVideoID(video);
   }
 
+  function createComment(comment) {
+    return (
+      <Comment
+        key={comment.id}
+        username={comment.username}
+        image={comment.image}
+        time={comment.time}
+        text={comment.text}
+        likes={comment.likes}
+        replies={comment.likes}
+      />
+    );
+  }
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/video?v=${currentVideoID}`)
+      .then((response) => response.json())
+      .then((response) => setVideoDetails(response))
+      .catch((err) => console.log(err));
+
+    fetch(`http://localhost:3000/comments?v=${currentVideoID}`)
+      .then((response) => response.json())
+      .then((response) => setComments(response))
+      .catch((err) => console.log(err));
+  }, [currentVideoID]);
+
   return (
     <div>
-      <input
-        onChange={handleInputChange}
-        type="text"
-        value={videoID}
-        placeholder="Enter video ID"
-      />
-      <button onClick={handleLoadVideoClick}>Load Video</button>
-      <div className="video">
-        <YoutubeEmbed videoID={currentVideoID} />
+      <div className="search-bar">
+        <i className="fab fa-youtube youtube-icon"></i>
+        <span className="title">YouTube With Comments</span>
+        <input
+          onChange={handleInputChange}
+          type="text"
+          value={videoID}
+          placeholder="Enter video ID"
+          className="search"
+        />
+        <button onClick={handleLoadVideoClick}>
+          <i className="fas fa-search search-icon"></i>
+        </button>
+      </div>
+      <div className="columns">
+        <div className="video">
+          <YoutubeEmbed videoID={currentVideoID} />
+          <VideoDetails videoDetails={videoDetails} />
+        </div>
+        <div className="comments">{comments.map(createComment)}</div>
       </div>
     </div>
   );
